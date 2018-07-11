db79ace4d52bd98350cfc114b979c76dd81261ee
Jacek Jackiewicz <jjackiewicz86@gmail.com>
2018-07-11 18:19:20 +0200

    Task 15.5

---
+++ components/App.js
@@ -0,0 +1,67 @@
+const GIPHY_PUB_KEY = 'dc6zaTOxFJmzC';
+const GIPHY_API_URL = 'https://api.giphy.com';
+
+App = React.createClass({
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
+    handleSearch: function(searchingText) {
+        this.setState({
+            loading: true
+        });
+        this.getGif(searchingText).then (gif => { 
+            this.setState({ 
+              loading: false, 
+              gif: gif, 
+              searchingText: searchingText 
+            });
+          });
+        },
+    getGif: function(searchingText) { 
+        return new Promise (
+          function (resolve, reject) {
+            let url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText; 
+            let xhr = new XMLHttpRequest(); 
+            xhr.onload = () => {
+              if (this.status === 200) {
+                let data = JSON.parse(xhr.responseText).data; 
+                let gif = {
+                  url: data.fixed_width_downsampled_url,
+                  sourceUrl: data.url
+                };
+                resolve(gif); 
+              }
+              reject(new Error(this.statusText));
+            };
+            xhr.onerror = function () {
+              reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+            };
+            xhr.open('GET', url);
+            xhr.send();
+          }
+        );
+      },
+    render: function() {
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+        return (
+            <div style={styles}>
+                <h1>GIF search</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <Search onSearch={this.handleSearch} />
+                <Gif 
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl} 
+                />
+            </div>
+        );
+    }
+});
